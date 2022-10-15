fbdcff44697f6cad0337628f5a5645629cf093c4
FidelisChidozie <113200990+FidelisChidozie@users.noreply.github.com>
2022-10-15 15:23:22 +0400

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 /**
  * main - entry point
@@ -9,10 +7,26 @@
  */
 int main(void)
 {
-	char a;
+	int i;
+	int j;
 
-	for (a = 'a'; a <= 'z' ; a++)
-		putchar(a);
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
